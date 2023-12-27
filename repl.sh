fbd369cea58f0603c779c5531546fdf3adf506fa
sidgharge <ghargesiddharth@gmail.com>
2023-12-27 15:35:11 +0530

    repl refactoring

--- repl.sh
+++ repl.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-mvn clean install -DskipTests
+set -e
+
+mvn -projects parser -am package -DskipTests
+
+clear
 
 java -cp parser/target/parser-1.0-SNAPSHOT.jar come.homeproects.jvmstudy.parser.Repl
 
